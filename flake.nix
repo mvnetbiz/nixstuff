6e7744e7812ba7e57bde5461d036407f152de441
Matt Votava <mvnetbiz@gmail.com>
2025-11-12 21:15:38 -0800

    auto import from pkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,24 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage ./mnvim.nix { };
-  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      packages = lib.genAttrs systems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        lib.mapAttrs # /
+          (n: v: pkgs.callPackage (./. + "/pkgs/${n}/package.nix") { })
+          (builtins.readDir ./pkgs)
+      );
+    };
 }
